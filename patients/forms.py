2231b0c5a0b3a19a9e71687e474628c4f776b812
Aslampa <93756533+LW1117@users.noreply.github.com>
2025-04-18 11:03:19 +0530

    Add forms.py

---
+++ patients/forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SelectField, SubmitField
+from wtforms.validators import DataRequired, Length, NumberRange, Email
+
+class PatientForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(min=2, max=100)])
+    age = IntegerField('Age', validators=[DataRequired(), NumberRange(min=1, max=120)])
+    gender = SelectField('Gender', choices=[('male', 'Male'), ('female', 'Female'), ('other', 'Other')], validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email(), Length(max=100)])
+    phone = StringField('Phone', validators=[DataRequired(), Length(min=7, max=15)])
+    location = StringField('Location', validators=[DataRequired(), Length(max=100)])
+    submit = SubmitField('Submit')
