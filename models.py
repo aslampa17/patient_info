3bc63e338335281f698cce04b2b91cc807aaeed3
Aslampa <93756533+LW1117@users.noreply.github.com>
2025-04-18 11:43:54 +0530

    Add date fields in model

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone
 
 db = SQLAlchemy()
 
@@ -10,3 +11,8 @@ class PatientInfo(db.Model):
     email = db.Column(db.String(100), nullable=False)
     phone = db.Column(db.String(15), nullable=False)
     location = db.Column(db.String(100))
+    created_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
+    updated_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
+
+    def __repr__(self):
+        return f"<PatientInfo {self.id}: {self.name}>"
